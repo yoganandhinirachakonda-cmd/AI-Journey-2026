ff5ff4855427da4e8326d3a69315d1b417d3576e
Yoga Nandhini <yoganandhinirachakonda@gmail.com>
2026-07-04 17:17:50 +0530

    Day 1: input and f-strings

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
    Day 0 - July 3, 2026
    Setup done
+
+     Day 1 - July 4, 2026: Learned input(), f-strings, variables. 
---
+++ day_1input.py
@@ -0,0 +1,4 @@
+# Day 1 - July 4, 2026
+first = input('Enter first name: ')
+last = input('Enter last name: ')
+print(f'{first} {last} is a coder')
